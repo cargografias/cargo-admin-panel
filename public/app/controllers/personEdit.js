e30f47e1d9c32e41017318091d42983b697d1882
Dan Calligaro <dancalligaro@gmail.com>
2015-07-16 18:43:23 -0300

    Add birth and death dates + indentation

--- public/app/controllers/personEdit.js
+++ public/app/controllers/personEdit.js
@@ -1,64 +1,105 @@
 angular.module('cargoNgApp')
 
- .controller('PersonEditController', function($scope, $modalInstance, $http, item) {
+.controller('PersonEditController', function($scope, $modalInstance, $http, item, $filter) {
+	
+	$scope.person = {};
+	loadPerson(item.id);
 
- 	$scope.person = {};
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
 
- 	$scope.person.name = item.name;
- 	if(item.given_name) $scope.person.given_name = item.given_name;
- 	if(item.family_name) $scope.person.family_name = item.family_name;
- 	if(item.gender) $scope.person.gender = item.gender;
- 	if(item.image) $scope.person.image = item.image;
- 	if(item.identifiers && item.identifiers.length > 0){
- 		if(item.identifiers[0].scheme) $scope.person.idtype = item.identifiers[0].scheme;
- 		if(item.identifiers[0].identifier) $scope.person.idnumber = item.identifiers[0].identifier;
+    $scope.birthUpdated = function() {
+        try {
+            $scope.person.birth_date = $filter('date')($scope.birthDT, 'yyyy-MM-dd')
+        } catch (ex) {
+            $scope.person.birth_date = null;
+        }
+    };
+
+    $scope.deathUpdated = function() {
+        try {
+            $scope.person.death_date = $filter('date')($scope.deathDT, 'yyyy-MM-dd')
+        } catch (ex) {
+            $scope.person.death_date = null;
+        }
+    };
+
+    $scope.openB = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.openedB = true;
+    };
+
+    $scope.openD = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.openedD = true;
+    };
+
+    $scope.calcName = function() {
+        $scope.person.name = ($scope.person.given_name || '') + ' ' + ($scope.person.family_name || '');
+    };
+
+    $scope.save = function() {
+        // window.__bootstrapData.popitKey
+        var personToSave = angular.extend({}, $scope.person);
+
+        if ($scope.idnumber || $scope.idtype) {
+            personToSave.identifiers = [{
+                identifier: $scope.idnumber,
+                scheme: $scope.idtype
+            }]
+        }
+
+        var url = "/proxy/persons/" + item.id;
+
+        $http({
+            method: 'PUT',
+            url: url,
+            data: personToSave,
+        }).success(function() {
+            $modalInstance.close();
+        }).error(function() {
+            console.log('Error saving person', arguments)
+            alert('Error saving person')
+        });
+
+    };
+
+    $scope.cancel = function() {
+        $modalInstance.close();
+    };
+
+ 	function loadPerson(personId){
+	 	var url = "https://" + window.__bootstrapData.user.popitUrl + ".popit.mysociety.org/api/v0.1/persons/" + personId + "?embed="
+	 	$http.get(url).then(function(response){
+	 		$scope.person = response.data.result;
+		    if ($scope.person.identifiers && $scope.person.identifiers.length > 0) {
+		        if ($scope.person.identifiers[0].scheme) $scope.idtype = $scope.person.identifiers[0].scheme;
+		        if ($scope.person.identifiers[0].identifier) $scope.idnumber = $scope.person.identifiers[0].identifier;
+		    }
+
+		    if($scope.person.birth_date){
+		    	try{
+		    		$scope.birthDT = moment($scope.person.birth_date, "YYYY-MM-DD").toDate()
+		    	}catch(ex){
+		    		console.log("not valid birth date")	
+		    	}
+		    }
+
+		    if($scope.person.death_date){
+		    	try{
+		    		$scope.deathDT = moment($scope.person.death_date, "YYYY-MM-DD").toDate()
+		    	}catch(ex){
+		    		console.log("not valid death date")	
+		    	}
+		    }
+
+	 	})
  	}
 
- 	console.log('this is the received item', item);
-
- 	$scope.calcName = function(){
- 		$scope.person.name = ($scope.person.given_name || '') + ' ' + ($scope.person.family_name || '');
- 	};
-
- 	$scope.save = function(){
- 		// window.__bootstrapData.popitKey
- 		var personToSave = {
- 			name: $scope.person.name, 
- 			given_name: $scope.person.given_name, 
- 			family_name: $scope.person.family_name,
- 			gender: $scope.person.gender,
- 			image: $scope.person.image,
- 		}
-
- 		if( $scope.person.idnumber || $scope.person.idtype){
- 			personToSave.identifiers = [
- 				{
- 					identifier: $scope.person.idnumber, 
- 					scheme: $scope.person.idtype
- 				}
- 			] 
- 		}
-
- 		var url = "/proxy/persons/" + item.id;
- 		
- 		$http({
- 			method: 'PUT',
- 			url: url, 
- 			data: personToSave, 
- 		}).success(function(){
-	 		$modalInstance.close();
- 		}).error(function(){
- 			console.log('Error saving person', arguments)
- 			alert('Error saving person')
- 		});
-	
-
-
- 	};
-
- 	$scope.cancel = function(){
- 		$modalInstance.close();
- 	};
-
- });
-
+});
